7da1026f1864309957b480089b9f2d03d2f32214
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2021-03-28 01:34:12 +0530

    rm old graph lib and add new one

--- components/dashboard.component.js
+++ components/dashboard.component.js
@@ -28,14 +28,21 @@ export const DashboardScreen = ({ navigation }) => {
             display: "flex",
             flexDirection: "row",
             alignItems: "center",
-            width: "90%",
+            width: "85%",
             justifyContent: "space-evenly",
             marginVertical: 20,
             padding: 10,
+            fontFamily: "JosefinSans_500Medium",
+            backgroundColor: "#f8f9fb",
           }}
         >
           <Avatar
-            style={{ width: 60, height: 60 }}
+            style={{
+              width: 60,
+              height: 60,
+              borderColor: "black",
+              borderWidth: 1,
+            }}
             source={require("../assets/photo.jpg")}
           />
           <Text
@@ -47,6 +54,16 @@ export const DashboardScreen = ({ navigation }) => {
             Welcome Back, John
           </Text>
         </Layout>
+        <Layout
+          style={{
+            backgroundColor: "#f8f9fb",
+            width: "80%",
+            display: "flex",
+            alignItems: "center",
+            borderColor: "#f4f4f4",
+            borderWidth: 2,
+          }}
+        ></Layout>
       </Layout>
     </>
   );
