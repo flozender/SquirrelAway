67cb0c45e1fbe61ed28fd4a73e79089bd8dcb6dc
rextivlan <affan.ali386@gmail.com>
2021-03-28 03:17:28 +0530

    Add Top Nav Component

---
+++ components/topNav.component.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  Icon,
+  Text,
+  TopNavigation,
+  TopNavigationAction,
+} from "@ui-kitten/components";
+
+export const TopNav = ({ title }) => {
+  const BackIcon = (props) => <Icon {...props} name="arrow-back" />;
+  const BackAction = () => <TopNavigationAction icon={BackIcon} />;
+
+  return (
+    <TopNavigation
+      title={(evaProps) => (
+        <Text
+          {...evaProps}
+          style={{ fontFamily: "JosefinSans_500Medium", fontSize: 25 }}
+        >
+          {title}
+        </Text>
+      )}
+      alignment="center"
+      accessoryLeft={BackAction}
+      style={{
+        backgroundColor: "#f8f9fb",
+        borderColor: "#c3d0d9",
+        borderWidth: 2,
+        marginTop: 3,
+      }}
+    />
+  );
+};
