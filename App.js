102ebf7131084b10e9754ac60d377e910c0b6e79
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2021-03-28 20:04:27 +0530

    remove .jsons

--- App.js
+++ App.js
@@ -6,24 +6,15 @@ import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import { AppNavigator } from "./components/navigation.component";
 import { default as theme } from "./theme.json";
-import { default as mapping } from "./mapping.json";
 
 import {
   useFonts,
-  JosefinSans_100Thin,
-  JosefinSans_200ExtraLight,
   JosefinSans_300Light,
   JosefinSans_400Regular,
   JosefinSans_500Medium,
   JosefinSans_600SemiBold,
   JosefinSans_700Bold,
-  JosefinSans_100Thin_Italic,
-  JosefinSans_200ExtraLight_Italic,
-  JosefinSans_300Light_Italic,
   JosefinSans_400Regular_Italic,
-  JosefinSans_500Medium_Italic,
-  JosefinSans_600SemiBold_Italic,
-  JosefinSans_700Bold_Italic,
 } from "@expo-google-fonts/josefin-sans";
 
 export default () => {
@@ -41,11 +32,7 @@ export default () => {
     return (
       <>
         <IconRegistry icons={EvaIconsPack} />
-        <ApplicationProvider
-          {...eva}
-          theme={{ ...eva.light, ...theme }}
-          customMapping={mapping}
-        >
+        <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
           <SafeAreaView
             style={{ flex: 1, marginTop: StatusBar.currentHeight || 0 }}
           >
