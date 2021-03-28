e77e8aef68ba41648f90d4228538a9bfa03a9d3d
rextivlan <affan.ali386@gmail.com>
2021-03-28 06:43:02 +0530

    Add Savings Page

--- components/navigation.component.js
+++ components/navigation.component.js
@@ -3,11 +3,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { AuthScreen } from "./auth.component";
 import { DashboardScreen } from "./dashboard.component";
+import { SavingsScreen } from "./savings.component";
 
 const { Navigator, Screen } = createStackNavigator();
 
 const HomeNavigator = () => (
   <Navigator headerMode="none">
+    <Screen name="Savings" component={SavingsScreen} />
     <Screen name="Auth" component={AuthScreen} />
     <Screen name="Dashboard" component={DashboardScreen} />
   </Navigator>
---
+++ components/savings.component.js
@@ -0,0 +1,144 @@
+import React from "react";
+import ProgressCircle from "react-native-progress-circle";
+import { Button, Layout, useTheme, Text } from "@ui-kitten/components";
+import { TopNav } from "./topNav.component";
+import { View } from "react-native";
+
+export const SavingsScreen = ({ navigation }) => {
+  const theme = useTheme();
+  const navigateSavings = () => {
+    navigation.navigate("Savings");
+  };
+
+  return (
+    <>
+      <TopNav title={"Savings"} />
+      <Layout
+        style={{
+          flex: 1,
+          alignItems: "center",
+        }}
+      >
+        <Layout
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            // justifyContent: "space-evenly",
+            marginVertical: 30,
+            padding: 10,
+            fontFamily: "JosefinSans_500Medium",
+            backgroundColor: "#f8f9fb",
+            borderColor: "#c3d0d9",
+            borderWidth: 2,
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "JosefinSans_500Medium",
+              fontSize: 18,
+            }}
+          >
+            Your Savings
+          </Text>
+          <Text
+            style={{ fontWeight: "bold", fontSize: 40, textAlign: "center" }}
+          >
+            $6637
+          </Text>
+        </Layout>
+        <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+          Your Saving Goals
+        </Text>
+      </Layout>
+      <View style={{ paddingBottom: 240, backgroundColor: "#fff" }}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            paddingBottom: 40,
+            backgroundColor: "#fff",
+          }}
+        >
+          <View>
+            <ProgressCircle
+              percent={30}
+              radius={50}
+              borderWidth={8}
+              color="#3399FF"
+              shadowColor="#999"
+              bgColor="#fff"
+            >
+              <Text style={{ fontSize: 20 }}>{"30%"}</Text>
+            </ProgressCircle>
+            <Text style={{ fontSize: 20, textAlign: "center" }}>New Car</Text>
+          </View>
+          <View>
+            <ProgressCircle
+              percent={66}
+              radius={50}
+              borderWidth={8}
+              color="red"
+              shadowColor="#999"
+              bgColor="#fff"
+            >
+              <Text style={{ fontSize: 20 }}>{"66%"}</Text>
+            </ProgressCircle>
+            <Text style={{ fontSize: 20, textAlign: "center" }}>
+              Text Books
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            backgroundColor: "#fff",
+          }}
+        >
+          <View>
+            <ProgressCircle
+              percent={100}
+              radius={50}
+              borderWidth={8}
+              color="#00FA9A"
+              shadowColor="#999"
+              bgColor="#fff"
+            >
+              <Text style={{ fontSize: 20 }}>{"100%"}</Text>
+            </ProgressCircle>
+            <Text style={{ fontSize: 20, textAlign: "center" }}>
+              Gift for Alex
+            </Text>
+          </View>
+          <View>
+            <ProgressCircle
+              percent={0}
+              radius={50}
+              borderWidth={8}
+              color="gray"
+              shadowColor="#999"
+              bgColor="#fff"
+            >
+              <Text
+                style={{
+                  fontSize: 40,
+                  fontWeight: "bold",
+                  color: "gray",
+                  alignSelf: "center",
+                }}
+              >
+                {"+"}
+              </Text>
+            </ProgressCircle>
+            <Text style={{ fontSize: 20, textAlign: "center" }}>New Goal</Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+};
